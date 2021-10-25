b70255fdf02be2dd4d9c000bae16c02cd50800e3
uttpal <kruttpal@gmail.com>
2021-10-25 23:24:06 +0530

    init

---
+++ src/main/java/com/modularitylabs/auxs/AuxsApplication.java
@@ -0,0 +1,13 @@
+package com.modularitylabs.auxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuxsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuxsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modularitylabs/auxs/AuxsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modularitylabs.auxs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuxsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
